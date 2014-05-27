4fe226267c5cdf53b4e9a1b8f1d847f6be489f1f
Bailey Seymour <b.seymour@me.com>
2014-05-27 17:52:46 -0400

    Update PFHeaderCell.mm

--- PFHeaderCell.mm
+++ PFHeaderCell.mm
@@ -1,5 +1,85 @@
-#import <Preferences/PSSpecifier.h>
-#import <Preferences/PSTableCell.h>
+//PSSpecifier.h
+@class NSMutableDictionary, NSDictionary, NSString, NSArray;
+
+typedef enum PSCellType {
+	PSGroupCell,
+	PSLinkCell,
+	PSLinkListCell,
+	PSListItemCell,
+	PSTitleValueCell,
+	PSSliderCell,
+	PSSwitchCell,
+	PSStaticTextCell,
+	PSEditTextCell,
+	PSSegmentCell,
+	PSGiantIconCell,
+	PSGiantCell,
+	PSSecureEditTextCell,
+	PSButtonCell,
+	PSEditTextViewCell,
+} PSCellType;
+
+@interface PSSpecifier : NSObject {
+@public
+	id target;
+	SEL getter;
+	SEL setter;
+	SEL action;
+	Class detailControllerClass;
+	PSCellType cellType;
+	Class editPaneClass;
+	UIKeyboardType keyboardType;
+	UITextAutocapitalizationType autoCapsType;
+	UITextAutocorrectionType autoCorrectionType;
+	int textFieldType;
+@private
+	NSString* _name;
+	NSArray* _values;
+	NSDictionary* _titleDict;
+	NSDictionary* _shortTitleDict;
+	id _userInfo;
+	NSMutableDictionary* _properties;
+}
+@property(retain) NSMutableDictionary* properties;
+@property(retain) NSString* name;
+@property(retain) id userInfo;
+@property(retain) id titleDictionary;
+@property(retain) id shortTitleDictionary;
+@property(retain) NSArray* values;
++(id)preferenceSpecifierNamed:(NSString*)title target:(id)target set:(SEL)set get:(SEL)get detail:(Class)detail cell:(PSCellType)cell edit:(Class)edit;
++(PSSpecifier*)groupSpecifierWithName:(NSString*)title;
++(PSSpecifier*)emptyGroupSpecifier;
++(UITextAutocapitalizationType)autoCapsTypeForString:(PSSpecifier*)string;
++(UITextAutocorrectionType)keyboardTypeForString:(PSSpecifier*)string;
+// inherited: -(id)init;
+-(id)propertyForKey:(NSString*)key;
+-(void)setProperty:(id)property forKey:(NSString*)key;
+-(void)removePropertyForKey:(NSString*)key;
+-(void)loadValuesAndTitlesFromDataSource;
+-(void)setValues:(NSArray*)values titles:(NSArray*)titles;
+-(void)setValues:(NSArray*)values titles:(NSArray*)titles shortTitles:(NSArray*)shortTitles;
+-(void)setupIconImageWithPath:(NSString*)path;
+// inherited: -(void)dealloc;
+// inherited: -(id)description;
+-(NSString*)identifier;
+-(void)setTarget:(id)target;
+-(void)setKeyboardType:(UIKeyboardType)type autoCaps:(UITextAutocapitalizationType)autoCaps autoCorrection:(UITextAutocorrectionType)autoCorrection;
+// -(int)titleCompare:(NSString*)compare;
+@end
+
+//PSTableCell.h
+@class PSSpecifier;
+
+@interface PSTableCell : UITableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier;
+
+@property (nonatomic, retain) id target;
+@property SEL action;
+
+@end
+
+
 #import "PFHeaderCell.h"
 //PFHeaderCell.mm by Pixel Fire http://pixelfire.baileyseymour.com
 
